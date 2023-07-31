2a96b49c8bdd64cc51cdaf2723226d57c2aea773
Grayson Shogan <graysontoddshogan@gmail.com>
2023-07-31 12:15:35 -0700

    wip

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const server = require('./api/server.js');
-
-const PORT = process.env.PORT || 9000;
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
-});
+module.exports = {
+  BCRYPT_ROUNDS: process.env.BCRYPT_ROUNDS || 8,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || 9000,
+  JWT_SECRET: process.env.JWT_SECRET || "shh",
+};
